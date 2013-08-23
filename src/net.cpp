f615d924d844fdc57ed1a09e7593fc637245427d
Christopher Dupee <dupee419@gmail.com>
2013-08-22 22:15:35 -0400

    changes

--- src/net.cpp
+++ src/net.cpp
@@ -997,7 +997,7 @@ void ThreadSocketHandler2(void* parg)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"andarazoroflove", "andarazoroflove.org"},
+    {"Seed1", "ip 71.181.74.10"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
